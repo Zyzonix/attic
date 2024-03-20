278315cc3f3d9e69f2cc537f8c21f0354f8929cc
zyzonix <zyzonix@gmail.com>
2024-03-20 20:31:46 +0100

    removed unused lines

--- ipfire-extensions/openvpn-notifier.py
+++ ipfire-extensions/openvpn-notifier.py
@@ -212,33 +212,21 @@ class mailHandler():
             mailText += "------"
             mailText += "<br><b>Certs that will expire soon (fewer than " + str(VALIDITYDAYS) + " days):</b><br>"
             mailText += "<table><tr><th>Certificate Name:</th><th>Expiring Date:</th><th>Days left until expired:</th></tr>"
-            for cert in certsExpiringSoon.keys():
-                TABCOUNT = "\t\t"
-                if len(cert) < 14: TABCOUNT = "\t\t\t"
-                if len(cert) < 6: TABCOUNT = "\t\t\t\t"
-                mailText += "<tr><td>" + cert + "</td><td>" + certsExpiringSoon[cert][1] + "</td><td>" + certsExpiringSoon[cert][0] + "</td></tr>"
+            for cert in certsExpiringSoon.keys(): mailText += "<tr><td>" + cert + "</td><td>" + certsExpiringSoon[cert][1] + "</td><td>" + certsExpiringSoon[cert][0] + "</td></tr>"
             mailText += "</table><br>"
 
         if certsExpired:
             mailText += "------"
             mailText += "<br><b>Certs that already expired:</b><br>"
             mailText += "<table><tr><th>Certificate Name:</th><th>Expiring Date:</th></tr>"
-            for cert in certsExpired.keys():
-                TABCOUNT = "\t\t"
-                if len(cert) < 14: TABCOUNT = "\t\t\t"
-                if len(cert) < 6: TABCOUNT = "\t\t\t\t"
-                mailText += "<tr><td>" + cert + "</td><td>" + certsExpired[cert] + "</td></tr>"
+            for cert in certsExpired.keys(): mailText += "<tr><td>" + cert + "</td><td>" + certsExpired[cert] + "</td></tr>"
             mailText += "</table><br>"
 
         if certsValid and SHOWVALID:
             mailText += "------"
             mailText += "<br>Valid certificates:<br>"
             mailText += "<table><tr><th>Certificate Name:</th><th>Expiring Date:</th><th>Days left until expired:</th></tr>"
-            for cert in certsValid.keys():
-                TABCOUNT = "\t\t"
-                if len(cert) < 14: TABCOUNT = "\t\t\t"
-                if len(cert) < 6: TABCOUNT = "\t\t\t\t"
-                mailText += "<tr><td>" + cert + "</td><td>" + certsValid[cert][1] + "</td><td>" + certsValid[cert][0] + "</td></tr>"
+            for cert in certsValid.keys(): mailText += "<tr><td>" + cert + "</td><td>" + certsValid[cert][1] + "</td><td>" + certsValid[cert][0] + "</td></tr>"
             mailText += "</table><br>"
 
         if certsCheckFailed:
